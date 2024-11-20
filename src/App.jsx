7adf07936a177b5ee151da92ebc1d7705def6d75
RugeFan <ruijiefan94@outlook.com>
2024-11-20 21:48:04 +1100

    Update with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Card from "./components/Card";
 const App = memo(() => {
   return <div className="bg-yellow h-screen w-full flex justify-center items-center">
     <Card />
+    <div class="attribution fixed bottom-0">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Rugee</a>.
+    </div>
   </div>;
 });
 
